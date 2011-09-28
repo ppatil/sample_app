0de9fd18eb5c1824a47a11b26ce2e85067d48f14
ppatil <pareshpatil@yahoo.com>
2011-09-28 00:15:01 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4f8bcfe78831f6fd9302d8b0c6e2f20664ef11fcf68181f5f115a1b7b2138396e2a4bdd9fcda3618eab969963a79c23c6559b41c379ef4a9f4913bc4d55ef2c0'
